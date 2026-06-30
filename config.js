a1490072849a5cd47dd72982fe0f3b49332faad4
faizaajmal <faizaajmal@users.noreply.github.com>
2026-06-30 22:22:12 +0000

    deploy: 024e4df56765d28f50196d1ce7bc3e8229dc998f

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const PEXELS_API_KEY = "IlZzOgSeqxNBOCA1aQ1ByaveACw6ojPednzyoG6i3R17ozN6K9MI2G0y";
-const UNSPLASH_API_KEY = "https://script.google.com/macros/s/AKfycbxO19pIDyoo-sQT91ohWle1XhJ8lBE6nPBGLHdvl7k7hQS6uv5EyrjhHoXCYfmyVoHiFg/exec
-";
+const CONFIG = {
+  "PEXELS_API_KEY": "IlZzOgSeqxNBOCA1aQ1ByaveACw6ojPednzyoG6i3R17ozN6K9MI2G0y",
+  "SHEETS_ENDPOINT": "https://script.google.com/macros/s/AKfycbxO19pIDyoo-sQT91ohWle1XhJ8lBE6nPBGLHdvl7k7hQS6uv5EyrjhHoXCYfmyVoHiFg/exec\n"
+};
